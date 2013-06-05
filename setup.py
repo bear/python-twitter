6a04a9c0fc26e3f8800e7ac8246496735ba47961
Mike Taylor <bear42@gmail.com>
2013-06-04 22:52:05 -0400

    changed version to 1.0 removed doc directory until we can update docs for v1.1 API added to package MANIFEST.in the testdata directory

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@
 '''The setup and build script for the python-twitter library.'''
 
 __author__ = 'python-twitter@googlegroups.com'
-__version__ = '1.0rc1'
+__version__ = '1.0'
 
 
 # The base package metadata to be used by both distutils and setuptools
