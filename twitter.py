217cc29276639bd310b276bcb207918866ef4745
Muthiah Annamalai <muthuspost@gmail.com>
2013-08-24 19:01:35 -0400

    cleanup code, for flags by @bear; then add a test case for ParseTweet

--- twitter.py
+++ twitter.py
@@ -4797,20 +4797,28 @@ class _FileCache(object):
     return os.path.sep.join(hashed_key[0:_FileCache.DEPTH])
 
 class ParseTweet:
+  # compile once on import
+  regexp ={"RT": "^RT","MT":r"^MT","ALNUM":r"(@[a-zA-Z0-9_]+)",
+           "HASHTAG":r"(#[\w\d]+)","URL":r"([http://]?[a-zA-Z\d\/]+[\.]+[a-zA-Z\d\/\.]+)"}
+  regexp = {key:re.compile(value) for key,value in regexp.items()}
   def __init__(self,timeline_owner,tweet):
-    ''' timeline_owner : twitter handle of user account. tweet - 140 chars from feed; object does all computation on construction'''      
-    self.Owner = timeline_owner;
-    self.tweet = tweet;
-    self.RT = False;         
-    self.tweet = tweet;
-    self.UserHandles = ParseTweet.getUserHandles(tweet);
-    self.Hashtags = ParseTweet.getHashtags(tweet);
-    self.URLs = ParseTweet.getURLs(tweet);
-    self.RT = ParseTweet.getAttributeRT(tweet);
-    self.MT = ParseTweet.getAttributeMT(tweet);
+    ''' timeline_owner : twitter handle of user account. tweet - 140 chars from feed; object does all computation on construction
+        properties: 
+        RT, MT - boolean
+        URLs - list of URL
+        Hashtags - list of tags
+    '''
+    self.Owner = timeline_owner
+    self.tweet = tweet
+    self.UserHandles = ParseTweet.getUserHandles(tweet)
+    self.Hashtags = ParseTweet.getHashtags(tweet)
+    self.URLs = ParseTweet.getURLs(tweet)
+    self.RT = ParseTweet.getAttributeRT(tweet)
+    self.MT = ParseTweet.getAttributeMT(tweet)
+    
     # additional intelligence
     if ( self.RT and len(self.UserHandles) > 0 ): #change the owner of tweet?
-      self.Owner = self.UserHandles[0];        
+      self.Owner = self.UserHandles[0]
     return
 
   def __str__(self):
@@ -4820,24 +4828,24 @@ class ParseTweet:
   @staticmethod
   def getAttributeRT( tweet ):
     """ see if tweet is a RT """
-    return re.search(r'^RT',tweet.strip()) != None
+    return re.search(ParseTweet.regexp["RT"],tweet.strip()) != None
 
   @staticmethod
   def getAttributeMT( tweet ):
     """ see if tweet is a MT """
-    return re.search(r'^MT',tweet.strip()) != None
+    return re.search(ParseTweet.regexp["MT"],tweet.strip()) != None
   
   @staticmethod
   def getUserHandles( tweet ):
     """ given a tweet we try and extract all user handles in order of occurrence"""
-    return re.findall(r'(@[a-zA-Z0-9_]+)',tweet);
+    return re.findall(ParseTweet.regexp["ALNUM"],tweet)
   
   @staticmethod
   def getHashtags( tweet ):
     """ return all hashtags"""
-    return re.findall(r'(#[\w\d]+)',tweet);
+    return re.findall(ParseTweet.regexp["HASHTAG"],tweet)
   
   @staticmethod
   def getURLs( tweet ):
     """ URL : [http://]?[\w\.?/]+"""
-    return re.findall(r'([http://]?[a-zA-Z\d\/]+[\.]+[a-zA-Z\d\/\.]+)',tweet);
+    return re.findall(ParseTweet.regexp["URL"],tweet)
