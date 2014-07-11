1a28a85c334a9c158db68af2b14580e13aa83b47
Mike Taylor <bear42@gmail.com>
2014-07-10 22:07:19 -0400

    updated setup.py to be wheel compatible, changed README to rst, removed requirements.txt and other config and copyright cleanups

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright 2007-2013 The Python-Twitter Developers
+# Copyright 2007-2014 The Python-Twitter Developers
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,61 +16,41 @@
 
 '''The setup and build script for the python-twitter library.'''
 
-__author__ = 'python-twitter@googlegroups.com'
-__version__ = '2.0'
+import os
 
+from setuptools import setup, find_packages
 
-# The base package metadata to be used by both distutils and setuptools
-METADATA = dict(
-  name = "python-twitter",
-  version = __version__,
-  packages = ['twitter'],
-  author='The Python-Twitter Developers',
-  author_email='python-twitter@googlegroups.com',
-  description='A Python wrapper around the Twitter API',
-  license='Apache License 2.0',
-  url='https://github.com/bear/python-twitter',
-  keywords='twitter api',
+def read(*paths):
+    """Build a file path from *paths* and return the contents."""
+    with open(os.path.join(*paths), 'r') as f:
+        return f.read()
+
+setup(
+    name='python-twitter',
+    version='2.0',
+    author='The Python-Twitter Developers',
+    author_email='python-twitter@googlegroups.com',
+    license='Apache License 2.0',
+    url='https://github.com/bear/python-twitter',
+    keywords='twitter api',
+    description='A Python wrapper around the Twitter API',
+    long_description=(read('README.rst') + '\n\n' +
+                      read('AUTHORS.rst') + '\n\n' +
+                      read('CHANGES')),
+    packages=find_packages(exclude=['tests*']),
+    install_requires = ['simplejson', 'requests', 'requests-oauthlib'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Communications :: Chat',
+        'Topic :: Internet',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+    ],
 )
-
-# Extra package metadata to be used only if setuptools is installed
-SETUPTOOLS_METADATA = dict(
-  install_requires = ['setuptools', 'simplejson', 'requests', 'requests_oauthlib'],
-  include_package_data = True,
-  classifiers = [
-    'Development Status :: 5 - Production/Stable',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: Apache Software License',
-    'Topic :: Software Development :: Libraries :: Python Modules',
-    'Topic :: Communications :: Chat',
-    'Topic :: Internet',
-  ],
-  test_suite = 'twitter_test.suite',
-)
-
-
-def Read(file):
-  return open(file).read()
-
-def BuildLongDescription():
-  return '\n'.join([Read('README.md'), Read('CHANGES')])
-
-def Main():
-  # Build the long_description from the README and CHANGES
-  METADATA['long_description'] = BuildLongDescription()
-
-  # Use setuptools if available, otherwise fallback and use distutils
-  try:
-    import setuptools
-    METADATA.update(SETUPTOOLS_METADATA)
-    setuptools.setup(**METADATA)
-  except ImportError:
-    import sys
-    sys.stderr.write("Could not import setuptools, using distutils")
-    sys.stderr.write("NOTE: You will need to install dependencies manualy")
-    import distutils.core
-    distutils.core.setup(**METADATA)
-
-
-if __name__ == '__main__':
-  Main()
