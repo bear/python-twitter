8279bade1f42da60c492751fd6dcbeaaf71bc212
Muthiah Annamalai <ezhillang@gmail.com>
2015-03-07 19:20:20 -0500

    1) Add Emoticons parsing to the Tweet API. 2) Add tests for emoticons in parse_tweet feed 3) Update Authors to reflect this contribution and original parse_tweet API

--- twitter_test.py
+++ twitter_test.py
@@ -651,15 +651,29 @@ class ParseTest(unittest.TestCase):
 
         data = twitter.ParseTweet("@twitter", hashtag_n_URL)
         self.assertEqual([len(data.Hashtags), len(data.URLs)], [2, 1])
+        self.assertEqual(len(data.Emoticon),0)
 
         url_only = u"""The #Rainbow #Nebula, 544,667 #lightyears away. pic.twitter.com/2A4wSUK25A""";
         data = twitter.ParseTweet("@twitter", url_only)
         self.assertEqual([data.MT, len(data.Hashtags), len(data.URLs)], [False, 3, 1])
+        self.assertEqual(len(data.Emoticon),0)
 
         url_handle = u"""RT ‏@BarackObama POTUS recommends Python-Twitter #unrelated picture pic.twitter.com/w8lFIfuUmI""";
         data = twitter.ParseTweet("@twitter", url_handle)
         self.assertEqual([data.RT, len(data.Hashtags), len(data.URLs), len(data.UserHandles)], [True, 1, 1, 1])
+        self.assertEqual(len(data.Emoticon),0)
 
+    def testEmoticon(self):
+        url_handle = u"""RT ‏@BarackObama POTUS recommends :-) Python-Twitter #unrelated picture pic.twitter.com/w8lFIfuUmI""";
+        data = twitter.ParseTweet("@twitter", url_handle)
+        self.assertEqual([data.RT, len(data.Hashtags), len(data.URLs), len(data.UserHandles)], [True, 1, 1, 1])
+        self.assertEqual(len(data.Emoticon),1)
+
+        url_handle = u"""RT @cats ^-^ cute! But kitty litter :-( #unrelated picture""";
+        data = twitter.ParseTweet("@cats", url_handle)
+        self.assertEqual([data.RT, len(data.Hashtags), len(data.URLs), len(data.UserHandles)], [True, 1, 0, 1])
+        self.assertEqual(len(data.Emoticon),2)
+        self.assertEqual(data.Emoticon,['^-^',':-('])
 
 class MockHTTPBasicAuthHandler(object):
     '''A mock replacement for HTTPBasicAuthHandler'''
