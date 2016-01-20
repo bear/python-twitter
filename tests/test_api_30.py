6136f7c564a53a347b62694704199eb932e7e0ef
Jeremy Low <jeremylow@gmail.com>
2016-01-19 20:52:01 -0500

    adds in passing tests without modification to API

--- tests/test_api_30.py
+++ tests/test_api_30.py
@@ -5,6 +5,11 @@ import sys
 import unittest
 
 import twitter
+
+import warnings
+
+warnings.filterwarnings('ignore', category=DeprecationWarning)
+
 import responses
 
 
@@ -33,6 +38,257 @@ class ApiTest(unittest.TestCase):
     def tearDown(self):
         sys.stderr = self._stderr
         pass
+
+    def testApiSetUp(self):
+        self.assertRaises(
+            twitter.TwitterError,
+            lambda: twitter.Api(consumer_key='test'))
+
+    def testSetAndClearCredentials(self):
+        api = twitter.Api()
+        api.SetCredentials(consumer_key='test',
+                           consumer_secret='test',
+                           access_token_key='test',
+                           access_token_secret='test')
+        self.assertEqual(api._consumer_key, 'test')
+        self.assertEqual(api._consumer_secret, 'test')
+        self.assertEqual(api._access_token_key, 'test')
+        self.assertEqual(api._access_token_secret, 'test')
+
+        api.ClearCredentials()
+
+        self.assertFalse(all([
+            api._consumer_key,
+            api._consumer_secret,
+            api._access_token_key,
+            api._access_token_secret
+        ]))
+
+    @responses.activate
+    def testApiRaisesAuthErrors(self):
+        responses.add(
+            responses.GET,
+            'https://api.twitter.com/1.1/search/tweets.json?count=15&result_type=mixed&q=python',
+            body='',
+            match_querystring=True,
+            status=200)
+        api = twitter.Api()
+        api.SetCredentials(consumer_key='test',
+                           consumer_secret='test',
+                           access_token_key='test',
+                           access_token_secret='test')
+        api._Api__auth = None
+        self.assertRaises(twitter.TwitterError, lambda: api.GetFollowers())
+
+    @responses.activate
+    def testGetHelpConfiguration(self):
+        with open('testdata/get_help_configuration.json') as f:
+            resp_data = f.read()
+        responses.add(
+            responses.GET,
+            'https://api.twitter.com/1.1/help/configuration.json',
+            body=resp_data,
+            status=200)
+        resp = self.api.GetHelpConfiguration()
+        self.assertEqual(resp.get('short_url_length_https'), 23)
+
+    @responses.activate
+    def testGetShortUrlLength(self):
+        with open('testdata/get_help_configuration.json') as f:
+            resp_data = f.read()
+        responses.add(
+            responses.GET,
+            'https://api.twitter.com/1.1/help/configuration.json',
+            body=resp_data,
+            status=200)
+        resp = self.api.GetShortUrlLength()
+        self.assertEqual(resp, 23)
+        resp = self.api.GetShortUrlLength(https=True)
+        self.assertEqual(resp, 23)
+
+    @responses.activate
+    def testGetSearch(self):
+        with open('testdata/get_search.json') as f:
+            resp_data = f.read()
+            responses.add(
+                responses.GET,
+                'https://api.twitter.com/1.1/search/tweets.json?count=15&result_type=mixed&q=python',
+                body=resp_data,
+                match_querystring=True,
+                status=200)
+        resp = self.api.GetSearch(term='python')
+        self.assertEqual(len(resp), 1)
+        self.assertTrue(type(resp[0]), twitter.Status)
+        self.assertEqual(resp[0].id, 674342688083283970)
+
+        self.assertRaises(
+            twitter.TwitterError,
+            lambda: self.api.GetSearch(since_id='test'))
+        self.assertRaises(
+            twitter.TwitterError,
+            lambda: self.api.GetSearch(max_id='test'))
+        self.assertRaises(
+            twitter.TwitterError,
+            lambda: self.api.GetSearch(term='test', count='test'))
+        self.assertFalse(self.api.GetSearch())
+
+    @responses.activate
+    def testGetSearchGeocode(self):
+        with open('testdata/get_search_geocode.json') as f:
+            resp_data = f.read()
+            responses.add(
+                responses.GET,
+                'https://api.twitter.com/1.1/search/tweets.json?result_type=mixed&count=15&geocode=37.781157%2C-122.398720%2C100mi&q=python',
+                body=resp_data,
+                match_querystring=True,
+                status=200)
+        resp = self.api.GetSearch(
+            term="python",
+            geocode=('37.781157', '-122.398720', '100mi'))
+        status = resp[0]
+        self.assertTrue(status, twitter.Status)
+        self.assertTrue(status.geo)
+        self.assertEqual(status.geo['type'], 'Point')
+
+    @responses.activate
+    def testGetUsersSearch(self):
+        with open('testdata/get_users_search.json') as f:
+            resp_data = f.read()
+            responses.add(
+                responses.GET,
+                'https://api.twitter.com/1.1/users/search.json?count=20&q=python',
+                body=resp_data,
+                match_querystring=True,
+                status=200)
+        resp = self.api.GetUsersSearch(term='python')
+        self.assertEqual(type(resp[0]), twitter.User)
+        self.assertEqual(len(resp), 20)
+        self.assertEqual(resp[0].id, 63873759)
+        self.assertRaises(twitter.TwitterError,
+                          lambda: self.api.GetUsersSearch(term='python',
+                                                          count='test'))
+
+    @responses.activate
+    def testGetTrendsCurrent(self):
+        with open('testdata/get_trends_current.json') as f:
+            resp_data = f.read()
+        responses.add(
+            responses.GET,
+            'https://api.twitter.com/1.1/trends/place.json?id=1',
+            body=resp_data,
+            match_querystring=True,
+            status=200)
+        resp = self.api.GetTrendsCurrent()
+        self.assertTrue(type(resp[0]) is twitter.Trend)
+
+    @responses.activate
+    def testGetHomeTimeline(self):
+        with open('testdata/get_home_timeline.json') as f:
+            resp_data = f.read()
+        responses.add(
+            responses.GET,
+            'https://api.twitter.com/1.1/statuses/home_timeline.json',
+            body=resp_data,
+            match_querystring=True,
+            status=200)
+        resp = self.api.GetHomeTimeline()
+        status = resp[0]
+        self.assertEqual(type(status), twitter.Status)
+        self.assertEqual(status.id, 674674925823787008)
+
+        self.assertRaises(
+            twitter.TwitterError,
+            lambda: self.api.GetHomeTimeline(count='literally infinity'))
+        self.assertRaises(
+            twitter.TwitterError,
+            lambda: self.api.GetHomeTimeline(count=4000))
+        self.assertRaises(
+            twitter.TwitterError,
+            lambda: self.api.GetHomeTimeline(max_id='also infinity'))
+        self.assertRaises(twitter.TwitterError,
+                          lambda: self.api.GetHomeTimeline(
+                              since_id='still infinity'))
+
+
+        # TODO: Get data for this call against which we can test exclusions.
+        responses.add(
+            responses.GET,
+            'https://api.twitter.com/1.1/statuses/home_timeline.json?count=100&max_id=674674925823787008&trim_user=1',
+            body=resp_data,
+            match_querystring=True,
+            status=200)
+        self.assertTrue(self.api.GetHomeTimeline(count=100,
+                                                 trim_user=True,
+                                                 max_id=674674925823787008))
+
+    @responses.activate
+    def testGetUserTimeline(self):
+        with open('testdata/get_user_timeline.json') as f:
+            resp_data = f.read()
+        responses.add(
+            responses.GET,
+            'https://api.twitter.com/1.1/statuses/user_timeline.json?user_id=673483',
+            body=resp_data,
+            match_querystring=True,
+            status=200)
+        resp = self.api.GetUserTimeline(user_id=673483)
+        self.assertTrue(type(resp[0]) is twitter.Status)
+        self.assertTrue(type(resp[0].user) is twitter.User)
+        self.assertEqual(resp[0].user.id, 673483)
+
+        responses.add(
+            responses.GET,
+            'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=dewitt',
+            body=resp_data,
+            match_querystring=True,
+            status=200)
+        resp = self.api.GetUserTimeline(screen_name='dewitt')
+        self.assertEqual(resp[0].id, 675055636267298821)
+        self.assertTrue(resp)
+
+    @responses.activate
+    def testGetRetweets(self):
+        with open('testdata/get_retweets.json') as f:
+            resp_data = f.read()
+        responses.add(
+            responses.GET,
+            'https://api.twitter.com/1.1/statuses/retweets/397.json',
+            body=resp_data,
+            match_querystring=True,
+            status=200)
+        resp = self.api.GetRetweets(statusid=397)
+        self.assertTrue(type(resp[0]) is twitter.Status)
+        self.assertTrue(type(resp[0].user) is twitter.User)
+
+    @responses.activate
+    def testGetRetweeters(self):
+        with open('testdata/get_retweeters.json') as f:
+            resp_data = f.read()
+        responses.add(
+            responses.GET,
+            'https://api.twitter.com/1.1/statuses/retweeters/ids.json?id=397',
+            body=resp_data,
+            match_querystring=True,
+            status=200)
+        resp = self.api.GetRetweeters(status_id=397)
+        self.assertTrue(type(resp) is list)
+        self.assertTrue(type(resp[0]) is int)
+
+    @responses.activate
+    def testGetBlocks(self):
+        with open('testdata/get_blocks.json') as f:
+            resp_data = f.read()
+        responses.add(
+            responses.GET,
+            'https://api.twitter.com/1.1/blocks/list.json?cursor=-1',
+            body=resp_data,
+            match_querystring=True,
+            status=200)
+        resp = self.api.GetBlocks()
+        self.assertTrue(type(resp) is list)
+        self.assertTrue(type(resp[0]) is twitter.User)
+        self.assertEqual(len(resp), 1)
+        self.assertEqual(resp[0].screen_name, 'RedScareBot')
 
     @responses.activate
     def testGetFriendIDs(self):
@@ -313,3 +569,222 @@ class ApiTest(unittest.TestCase):
         else:
             self.assertTrue(type(resp[0]) is unicode)
         self.assertEqual(len(resp), 5000)
+
+    @responses.activate
+    def testUsersLookup(self):
+        with open('testdata/users_lookup.json') as f:
+            resp_data = f.read()
+        responses.add(
+            responses.GET,
+            'https://api.twitter.com/1.1/users/lookup.json?user_id=718443',
+            body=resp_data,
+            match_querystring=True,
+            status=200)
+        resp = self.api.UsersLookup(user_id=[718443])
+        self.assertTrue(type(resp) is list)
+        self.assertEqual(len(resp), 1)
+        user = resp[0]
+        self.assertTrue(type(user) is twitter.User)
+        self.assertEqual(user.screen_name, 'kesuke')
+        self.assertEqual(user.id, 718443)
+
+    @responses.activate
+    def testGetUser(self):
+        with open('testdata/get_user.json') as f:
+            resp_data = f.read()
+        responses.add(
+            responses.GET,
+            'https://api.twitter.com/1.1/users/show.json?user_id=718443',
+            body=resp_data,
+            match_querystring=True,
+            status=200)
+        resp = self.api.GetUser(user_id=718443)
+        self.assertTrue(type(resp) is twitter.User)
+        self.assertEqual(resp.screen_name, 'kesuke')
+        self.assertEqual(resp.id, 718443)
+
+    @responses.activate
+    def testGetDirectMessages(self):
+        with open('testdata/get_direct_messages.json') as f:
+            resp_data = f.read()
+        responses.add(
+            responses.GET,
+            'https://api.twitter.com/1.1/direct_messages.json',
+            body=resp_data,
+            match_querystring=True,
+            status=200)
+        resp = self.api.GetDirectMessages()
+        self.assertTrue(type(resp) is list)
+        direct_message = resp[0]
+        self.assertTrue(type(direct_message) is twitter.DirectMessage)
+        self.assertEqual(direct_message.id, 678629245946433539)
+
+    @responses.activate
+    def testGetSentDirectMessages(self):
+        with open('testdata/get_sent_direct_messages.json') as f:
+            resp_data = f.read()
+        responses.add(
+            responses.GET,
+            'https://api.twitter.com/1.1/direct_messages/sent.json',
+            body=resp_data,
+            match_querystring=True,
+            status=200)
+        resp = self.api.GetSentDirectMessages()
+        self.assertTrue(type(resp) is list)
+        direct_message = resp[0]
+        self.assertTrue(type(direct_message) is twitter.DirectMessage)
+        self.assertEqual(direct_message.id, 678629283007303683)
+        self.assertTrue([dm.sender_screen_name == 'notinourselves' for dm in resp])
+
+    @responses.activate
+    def testGetFavorites(self):
+        with open('testdata/get_favorites.json') as f:
+            resp_data = f.read()
+        responses.add(
+            responses.GET,
+            'https://api.twitter.com/1.1/favorites/list.json?include_entities=True',
+            body=resp_data,
+            match_querystring=True,
+            status=200)
+        resp = self.api.GetFavorites()
+        self.assertTrue(type(resp) is list)
+        fav = resp[0]
+        self.assertEqual(fav.id, 677180133447372800)
+        self.assertIn("Extremely", fav.text)
+
+    @responses.activate
+    def testGetMentions(self):
+        with open('testdata/get_mentions.json') as f:
+            resp_data = f.read()
+        responses.add(
+            responses.GET,
+            'https://api.twitter.com/1.1/statuses/mentions_timeline.json',
+            body=resp_data,
+            match_querystring=True,
+            status=200)
+        resp = self.api.GetMentions()
+        self.assertTrue(type(resp) is list)
+        self.assertTrue([type(mention) is twitter.Status for mention in resp])
+        self.assertEqual(resp[0].id, 676148312349609985)
+
+    @responses.activate
+    def testGetListTimeline(self):
+        with open('testdata/get_list_timeline.json') as f:
+            resp_data = f.read()
+        responses.add(
+            responses.GET,
+            'https://api.twitter.com/1.1/lists/statuses.json?slug=space-bots&owner_screen_name=inky',
+            body=resp_data,
+            match_querystring=True,
+            status=200)
+        resp = self.api.GetListTimeline(list_id=None,
+                                        slug='space-bots',
+                                        owner_screen_name='inky')
+        self.assertTrue(type(resp) is list)
+        self.assertTrue([type(status) is twitter.Status for status in resp])
+        self.assertEqual(resp[0].id, 677891843946766336)
+
+        self.assertRaises(
+            twitter.TwitterError,
+            lambda: self.api.GetListTimeline(
+                list_id=None,
+                slug=None,
+                owner_id=None))
+        self.assertRaises(
+            twitter.TwitterError,
+            lambda: self.api.GetListTimeline(
+                list_id=None,
+                slug=None,
+                owner_screen_name=None))
+
+    @responses.activate
+    def testPostUpdate(self):
+        with open('testdata/post_update.json') as f:
+            resp_data = f.read()
+        responses.add(
+            responses.POST,
+            'https://api.twitter.com/1.1/statuses/update.json',
+            body=resp_data,
+            status=200)
+        post = self.api.PostUpdate(
+            status="blah Longitude coordinate of the tweet in degrees.")
+        self.assertTrue(type(post) is twitter.Status)
+        self.assertEqual(
+            post.text, "blah Longitude coordinate of the tweet in degrees.")
+        self.assertTrue(post.geo is None)
+        self.assertEqual(post.user.screen_name, 'notinourselves')
+
+    @responses.activate
+    def testPostUpdateExtraParams(self):
+        with open('testdata/post_update_extra_params.json') as f:
+            resp_data = f.read()
+        responses.add(
+            responses.POST,
+            'https://api.twitter.com/1.1/statuses/update.json',
+            body=resp_data,
+            status=200)
+        post = self.api.PostUpdate(
+            status="Not a dupe. Longitude coordinate of the tweet in degrees.",
+            in_reply_to_status_id=681496308251754496,
+            latitude=37.781157,
+            longitude=-122.398720,
+            place_id="1",
+            display_coordinates=True,
+            trim_user=True)
+        self.assertEqual(post.in_reply_to_status_id, 681496308251754496)
+        self.assertIsNotNone(post.coordinates)
+
+    @responses.activate
+    def testVerifyCredentials(self):
+        with open('testdata/verify_credentials.json') as f:
+            resp_data = f.read()
+        responses.add(
+            responses.GET,
+            '{0}/account/verify_credentials.json'.format(self.api.base_url),
+            body=resp_data,
+            status=200)
+
+        resp = self.api.VerifyCredentials()
+        self.assertEqual(type(resp), twitter.User)
+        self.assertEqual(resp.name, 'notinourselves')
+
+    @responses.activate
+    def testUpdateBanner(self):
+        responses.add(
+            responses.POST,
+            '{0}/account/update_profile_banner.json'.format(self.api.base_url),
+            body=b'',
+            status=201
+        )
+        resp = self.api.UpdateBanner(image='testdata/168NQ.jpg')
+        self.assertTrue(resp)
+
+    @responses.activate
+    def testUpdateBanner422Error(self):
+        responses.add(
+            responses.POST,
+            '{0}/account/update_profile_banner.json'.format(self.api.base_url),
+            body=b'',
+            status=422
+        )
+        self.assertRaises(
+            twitter.TwitterError,
+            lambda: self.api.UpdateBanner(image='testdata/168NQ.jpg')
+        )
+        try:
+            self.api.UpdateBanner(image='testdata/168NQ.jpg')
+        except twitter.TwitterError as e:
+            self.assertTrue("The image could not be resized or is too large." in str(e))
+
+    @responses.activate
+    def testUpdateBanner400Error(self):
+        responses.add(
+            responses.POST,
+            '{0}/account/update_profile_banner.json'.format(self.api.base_url),
+            body=b'',
+            status=400
+        )
+        try:
+            self.api.UpdateBanner(image='testdata/168NQ.jpg')
+        except twitter.TwitterError as e:
+            self.assertTrue("Image data could not be processed" in str(e))
