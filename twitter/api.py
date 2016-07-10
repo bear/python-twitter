40aa8955c0c91f4abeab2aae3a6a11e38dbb1846
Zeta Syanthis <zeta@zetasyanthis.org>
2016-07-10 15:03:56 -0700

    Actually, we need max...

--- twitter/api.py
+++ twitter/api.py
@@ -4823,7 +4823,7 @@ class Api(object):
 
             if limit.remaining == 0:
                 try:
-                    time.sleep(min(int(limit.reset - time.time()), 0))
+                    time.sleep(max(int(limit.reset - time.time()), 0))
                 except ValueError:
                     pass
 
