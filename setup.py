f4d9161adb44b1fab7a6d88de00b4ddcf196fd55
Louis Sautier <sautier.louis@gmail.com>
2018-06-19 22:18:15 +0200

    Remove unnecessary pytest-runner from install_requires

--- setup.py
+++ setup.py
@@ -57,7 +57,6 @@ setup(
     packages=find_packages(exclude=('tests', 'docs')),
     platforms=['Any'],
     install_requires=['future', 'requests', 'requests-oauthlib'],
-    setup_requires=['pytest-runner'],
     tests_require=['pytest'],
     keywords='twitter api',
     classifiers=[
