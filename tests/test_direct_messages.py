188997834fba9660f53441e632cfa73239858bb0
Jeremy Low <jeremylow@gmail.com>
2018-12-31 10:06:18 -0500

    fix error in DM tests. update pep8 section for linter

--- tests/test_direct_messages.py
+++ tests/test_direct_messages.py
@@ -3,24 +3,15 @@
 
 from __future__ import unicode_literals, print_function
 
-import json
-import os
 import re
-import sys
-from tempfile import NamedTemporaryFile
-import unittest
-try:
-    from unittest.mock import patch
-except ImportError:
-    from mock import patch
-import warnings
 
 import twitter
 
 import responses
 from responses import GET, POST
 
-DEFAULT_URL = re.compile(r'https?://.*\.twitter.com/1\.1/.*')
+DEFAULT_BASE_URL = re.compile(r'https?://api\.twitter.com/1\.1/.*')
+DEFAULT_UPLOAD_URL = re.compile(r'https?://upload\.twitter.com/1\.1/.*')
 
 global api
 api = twitter.Api('test', 'test', 'test', 'test', tweet_mode='extended')
@@ -30,7 +21,7 @@ api = twitter.Api('test', 'test', 'test', 'test', tweet_mode='extended')
 def test_get_direct_messages():
     with open('testdata/direct_messages/get_direct_messages.json') as f:
         resp_data = f.read()
-    responses.add(GET, DEFAULT_URL, body=resp_data)
+    responses.add(GET, DEFAULT_BASE_URL, body=resp_data)
 
     resp = api.GetDirectMessages(count=1, page=1)
     direct_message = resp[0]
@@ -49,7 +40,7 @@ def test_get_direct_messages():
 def test_get_sent_direct_messages():
     with open('testdata/direct_messages/get_sent_direct_messages.json') as f:
         resp_data = f.read()
-    responses.add(GET, DEFAULT_URL, body=resp_data)
+    responses.add(GET, DEFAULT_BASE_URL, body=resp_data)
 
     resp = api.GetSentDirectMessages(count=1, page=1)
     direct_message = resp[0]
@@ -61,7 +52,7 @@ def test_get_sent_direct_messages():
 @responses.activate
 def test_post_direct_message():
     with open('testdata/direct_messages/post_post_direct_message.json', 'r') as f:
-        responses.add(POST, DEFAULT_URL, body=f.read())
+        responses.add(POST, DEFAULT_BASE_URL, body=f.read())
     resp = api.PostDirectMessage(user_id='372018022',
                                  text='hello')
     assert isinstance(resp, twitter.DirectMessage)
@@ -71,10 +62,13 @@ def test_post_direct_message():
 @responses.activate
 def test_post_direct_message_with_media():
     with open('testdata/direct_messages/post_post_direct_message.json', 'r') as f:
-        responses.add(POST, DEFAULT_URL, body=f.read())
+        responses.add(POST, DEFAULT_BASE_URL, body=f.read())
+    with open('testdata/post_upload_chunked_INIT.json') as f:
+        responses.add(POST, DEFAULT_UPLOAD_URL, body=f.read())
+
     resp = api.PostDirectMessage(user_id='372018022',
                                  text='hello',
-                                 media_file_path='testdata/media/happy.png',
+                                 media_file_path='testdata/media/happy.jpg',
                                  media_type='dm_image')
     assert isinstance(resp, twitter.DirectMessage)
     assert resp.text == 'hello'
@@ -83,7 +77,7 @@ def test_post_direct_message_with_media():
 @responses.activate
 def test_destroy_direct_message():
     with open('testdata/direct_messages/post_destroy_direct_message.json', 'r') as f:
-        responses.add(POST, DEFAULT_URL, body=f.read())
+        responses.add(POST, DEFAULT_BASE_URL, body=f.read())
     resp = api.DestroyDirectMessage(message_id=855194351294656515)
 
     assert isinstance(resp, twitter.DirectMessage)
